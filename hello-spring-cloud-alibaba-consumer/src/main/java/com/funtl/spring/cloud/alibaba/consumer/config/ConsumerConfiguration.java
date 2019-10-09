80e9202a14a83b68a17d4ab773a948d0375a84dd
asus <13101225226@163.com>
2019-10-09 15:16:57 +0800

    spring-cloud-alibaba basic

---
+++ hello-spring-cloud-alibaba-consumer/src/main/java/com/funtl/spring/cloud/alibaba/consumer/config/ConsumerConfiguration.java
@@ -0,0 +1,15 @@
+package com.funtl.spring.cloud.alibaba.consumer.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ConsumerConfiguration {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
